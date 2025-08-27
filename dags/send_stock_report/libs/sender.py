daa42aa7a9b628fa64bd87a7306d53373532bfdf
litwein <nicklitvinow@gmail.com>
2025-08-27 18:52:40 +0300

    send_stock_report: extract SQL to libs/constants.py; init SMTP connection via get_conn()

--- dags/send_stock_report/libs/sender.py
+++ dags/send_stock_report/libs/sender.py
@@ -3,46 +3,14 @@ import os
 import pandas as pd
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.smtp.hooks.smtp import SmtpHook
+from send_stock_report.libs.constants import STOCK_REPORT_SQL
 
 def get_stock_report_df(pg_conn_id: str) -> pd.DataFrame:
     pg_hook = PostgresHook(postgres_conn_id=pg_conn_id)
     conn = pg_hook.get_conn()
     logging.info("Fetching stock report via SQL...")
 
-    sql_query = """
-    WITH stocks AS (
-        SELECT
-          ean,
-          SUM(COALESCE(avail, 0)) AS avail
-        FROM
-          si.stock_for_customer
-        GROUP BY ean
-      ),
-      pl AS (
-        SELECT DISTINCT
-          ean,
-          description,
-          9 AS id
-        FROM md.price_list
-        WHERE description != 'NaN'
-        UNION
-        SELECT
-          ean::varchar,
-          description,
-          1000000
-        FROM
-          si.ean_add
-      )
-      SELECT
-        p.ean::numeric,
-        p.description,
-        ROUND(COALESCE(s.avail, 0))::numeric
-      FROM
-        pl p
-        LEFT JOIN stocks s ON (p.ean = s.ean)
-      ORDER BY p.id;
-    """
-    report_df = pd.read_sql_query(sql_query, conn)
+    report_df = pd.read_sql_query(STOCK_REPORT_SQL, conn)
     logging.info("Successfully fetched %s rows.", len(report_df))
     
     return report_df
@@ -63,6 +31,7 @@ def send_report_by_email(report_df: pd.DataFrame, recipients: dict, tmp_dir: str
     bcc = recipients.get('bcc') or []
 
     smtp_hook = SmtpHook('smtp_sys_tech')
+    smtp_hook.get_conn()
     smtp_hook.send_email_smtp(
         to=to,
         cc=cc,
