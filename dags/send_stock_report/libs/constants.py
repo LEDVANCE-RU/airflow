48f4c59e48f15ce09bc91fbc5cad24fd2642bda1
Nick Litwinow <44932745+NickLitwinow@users.noreply.github.com>
2025-08-28 12:47:33 +0300

    send_stock_report (#5)
    * send_stock_report: add DAG and email recipients to/cc/bcc with default Variable.get
    
    * send_stock_report: use Variable.get default param; require non-empty JSON {to,cc,bcc}; pass cc/bcc to email
    
    * send_stock_report: use SmtpHook(smtp_sys_tech); inline CTE SQL; relax recipients validation (no mandatory to)
    
    * send_stock_report: extract SQL to libs/constants.py; init SMTP connection via get_conn()

---
+++ dags/send_stock_report/libs/constants.py
@@ -0,0 +1,33 @@
+STOCK_REPORT_SQL = """
+WITH stocks AS (
+    SELECT
+      ean,
+      SUM(COALESCE(avail, 0)) AS avail
+    FROM
+      si.stock_for_customer
+    GROUP BY ean
+  ),
+  pl AS (
+    SELECT DISTINCT
+      ean,
+      description,
+      9 AS id
+    FROM md.price_list
+    WHERE description != 'NaN'
+    UNION
+    SELECT
+      ean::varchar,
+      description,
+      1000000
+    FROM
+      si.ean_add
+  )
+  SELECT
+    p.ean::numeric,
+    p.description,
+    ROUND(COALESCE(s.avail, 0))::numeric
+  FROM
+    pl p
+    LEFT JOIN stocks s ON (p.ean = s.ean)
+  ORDER BY p.id;
+"""
