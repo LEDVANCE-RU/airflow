8f505ab7121a756d3e2989449793a3b150c8dee5
Nick Litwinow <44932745+NickLitwinow@users.noreply.github.com>
2025-10-09 15:05:16 +0300

    Update dags/process_cbr_currency_rates/libs/mapping.py
    Co-authored-by: amid-flice <66000941+amid-flice@users.noreply.github.com>

--- dags/process_cbr_currency_rates/libs/mapping.py
+++ dags/process_cbr_currency_rates/libs/mapping.py
@@ -7,16 +7,20 @@ class Field:
     type: str
 
 
-class CbrFieldsMap:
+class CbrFieldsMap(StrEnum):
+    CURRENCY = 'currency'
+    RATE_RUB = 'rate_rub'
+    DATE = 'date'
+
     @classmethod
-    def dest_map(cls) -> dict[str, Field]:
+    def dest_map(cls) -> dict[str, 'Field']:
         return {
-            'currency': Field('currency', 'character varying(20)'),
-            'rate_rub': Field('rate_rub', 'numeric(20, 4)'),
-            'date': Field('date', 'date'),
+            cls.CURRENCY: Field('currency', 'character varying(20)'),
+            cls.RATE_RUB: Field('rate_rub', 'numeric(20, 4)'),
+            cls.DATE: Field('date', 'date'),
         }
 
     @classmethod
     def dest_columns(cls) -> list[str]:
-        return list(cls.dest_map().keys())
+        return [field.value for field in cls]
 
