20a6e692efc2a655a97ea419156f12e6aaa5c8d6
litwein <nicklitvinow@gmail.com>
2025-10-16 15:56:31 +0300

    Feature/test_send_stock_report: resolve email sending issues including type errors, invalid addresses, and MIME handling

--- dags/test_send_stock_report/libs/sender.py
+++ dags/test_send_stock_report/libs/sender.py
@@ -1,11 +1,8 @@
 import logging
 import os
-import re
+import mimetypes
 import pandas as pd
 from datetime import datetime
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.application import MIMEApplication
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.smtp.hooks.smtp import SmtpHook
 from test_send_stock_report.libs.constants import STOCK_REPORT_SQL
@@ -35,31 +32,21 @@ def send_report_by_email(report_df: pd.DataFrame, recipients: dict, tmp_dir: str
     cc = recipients.get('cc') or []
     bcc = recipients.get('bcc') or []
 
-    msg = MIMEMultipart()
-    msg['Subject'] = 'АО "ЛЕДВАНС": Остатки на складе на текущую дату'
-    msg['From'] = 'smtp_sys_tech'
-    msg['To'] = ', '.join(to) if isinstance(to, list) else to
-    if cc:
-        msg['Cc'] = ', '.join(cc) if isinstance(cc, list) else cc
-
-    html_body = (
-        "<p>Добрый день,</p>" 
-        "<p>Письмо было сформировано и отправлено автоматически. Просьба не отвечать на данное письмо.</p>"
-        "<p>По всем возникающим вопросам Вы можете обращаться к ответственному сотруднику отдела по работе с клиентами АО «ЛЕДВАНС»</p>"
-    )
-    msg.attach(MIMEText(html_body, 'html'))
-
-    with open(filepath, 'rb') as f:
-        attachment = MIMEApplication(f.read(), _subtype='vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-        attachment.add_header('Content-Disposition', 'attachment', filename=filename)
-        msg.attach(attachment)
+    mimetypes.add_type('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', '.xlsx', strict=False)
 
     smtp_hook = SmtpHook('smtp_sys_tech')
-    smtp_conn = smtp_hook.get_conn()
-    
-    all_recipients = to + cc + bcc
-    smtp_client = smtp_conn.smtp_client
-    smtp_client.sendmail(msg['From'], all_recipients, msg.as_string())
-    smtp_client.quit()
-    
+    smtp_hook.get_conn()
+    smtp_hook.send_email_smtp(
+        to=to,
+        cc=cc,
+        bcc=bcc,
+        subject='АО "ЛЕДВАНС": Остатки на складе на текущую дату',
+        html_content=(
+            "<p>Добрый день,</p>"
+            "<p>Письмо было сформировано и отправлено автоматически. Просьба не отвечать на данное письмо.</p>"
+            "<p>По всем возникающим вопросам Вы можете обращаться к ответственному сотруднику отдела по работе с клиентами АО «ЛЕДВАНС»</p>"
+        ),
+        files=[filepath],
+        mime_subtype='mixed',
+    )
     logging.info("Email sent to: to=%s cc=%s bcc=%s", to, cc, bcc)
