aafecf77ef465d6c11482b05ebc1c22e2d8c8099
D.Kosilov <amidflice@gmail.com>
2025-10-17 18:51:23 +0300

    Creating DB model.

---
+++ models/onec_extract/constants.py
@@ -0,0 +1,18 @@
+import uuid
+from enum import StrEnum
+
+WAREHOUSE_OF_GOODS_UUID = uuid.UUID('c3c4db62-d07b-11ee-a0f7-9b35e1ceb5f3')
+
+class IcLifecycleStatus(StrEnum):
+    NEW = 'NEW - Новинка'
+    REG = 'REG - Регулярная'
+    EOL = 'EOL - Выводится'
+    ARC = 'ARC - Архив'
+
+    @classmethod
+    def active_statuses(cls):
+        return [
+            cls.NEW,
+            cls.REG,
+            cls.EOL
+        ]
