88c8a7a1e4c00b13e9f09f737b57fd3a657ee1ca
D.Kosilov <amidflice@gmail.com>
2026-01-22 18:44:49 +0300

    DAG `sync_marketprovider_to_pg`: * Extended mapping.

--- plugins/api/marketprovider/mapping.py
+++ plugins/api/marketprovider/mapping.py
@@ -29,31 +29,43 @@ class ProductField(StrEnum):
 
 class ProductAttrGroupName(StrEnum):
     BASE = 'Базовые характеристики'
-    HOUSING = 'Корпус лампы'
-    MARKETING = 'Маркетинг'
-    PRICE_STATUS = 'Прайс-статус'
+    CERT_004 = 'Сертификат ТР 004 020'
+    CERT_037 = 'Декларация 037'
+    LAMP_HOUSING = 'Корпус лампы'
+    LIGHT_HOUSING = 'Корпус светильника'
     LAMP_ELECTRICAL_CHARACTERISTICS = 'Электрические характеристики лампа'
+    MARKETING = 'Маркетинг'
     OPTICAL_CHARACTERISTICS = 'Оптические характеристики'
+    PRICE_STATUS = 'Прайс-статус'
+    PROTECTION = 'Защита'
     QUALITY = 'Качество'
     WARRANTY = 'Гарантия'
 
 
 class ProductAttrName:
     NAME = (ProductAttrGroupName.BASE, 'Название продукта')
+    BRAND_NAME = (ProductAttrGroupName.BASE, 'Бренд')
     MAIN_IMAGE = (ProductAttrGroupName.BASE, 'Главное изображение')
-    PREDECESSOR = (ProductAttrGroupName.BASE, 'Предшественник')
     ORIGIN_COUNTRY = (ProductAttrGroupName.BASE, 'Страна производства')
     INNER_CODE = (ProductAttrGroupName.BASE, 'Внутренний артикул')
     VENDOR_CODE = (ProductAttrGroupName.BASE, 'Артикул производителя')
     EAN_UPC = (ProductAttrGroupName.BASE, 'EAN / UPC')
 
+    CERT_004_NUM = (ProductAttrGroupName.CERT_004, 'Номер сертификата ТР 004/020')
 
-    SERIES = (ProductAttrGroupName.HOUSING, 'Серия L4L')
-    BULB = (ProductAttrGroupName.HOUSING, 'Колба')
-    HOUSING_MATERIAL = (ProductAttrGroupName.HOUSING, 'Материал корпуса')
-    LAMP_CAP = (ProductAttrGroupName.HOUSING, 'Цоколь')
+    CERT_037_NUM = (ProductAttrGroupName.CERT_037, 'Номер декларации 037')
+
+    SERIES_L4L = (ProductAttrGroupName.LAMP_HOUSING, 'Серия L4L')
+    BULB = (ProductAttrGroupName.LAMP_HOUSING, 'Колба')
+    HOUSING_MATERIAL = (ProductAttrGroupName.LAMP_HOUSING, 'Материал корпуса')
+    LAMP_TYPE = (ProductAttrGroupName.LAMP_HOUSING, 'Вид лампы')
+    LAMP_CAP = (ProductAttrGroupName.LAMP_HOUSING, 'Цоколь')
+    HOUSING_COLOR = (ProductAttrGroupName.LAMP_HOUSING, 'Цвет корпуса')
+
+    MOUNTING_TYPE = (ProductAttrGroupName.LIGHT_HOUSING, 'Способ монтажа')
 
     MARKETING_NAME = (ProductAttrGroupName.MARKETING, 'Маркетинговое наименование')
+    MARKETING_SERIES = (ProductAttrGroupName.MARKETING, 'Маркетинговая серия')
 
     POWER = (ProductAttrGroupName.LAMP_ELECTRICAL_CHARACTERISTICS, 'Мощность, Вт')
     VOLTAGE = (ProductAttrGroupName.LAMP_ELECTRICAL_CHARACTERISTICS, 'Входное напряжение AC, В')
@@ -63,6 +75,10 @@ class ProductAttrName:
     DIMMABLE = (ProductAttrGroupName.OPTICAL_CHARACTERISTICS, 'Диммирование')
     BEAM_ANGLE = (ProductAttrGroupName.OPTICAL_CHARACTERISTICS, 'Угол пучка, °')
     COLOR_RENDERING_INDEX = (ProductAttrGroupName.OPTICAL_CHARACTERISTICS, 'Индекс цветопередачи Ra, ≥')
+
+    PREDECESSOR = (ProductAttrGroupName.PRICE_STATUS, 'Предшественник')
+
+    IP_CLASS = (ProductAttrGroupName.PROTECTION, 'Степень защиты IP')
 
     LIFESPAN = (ProductAttrGroupName.QUALITY, 'Срок службы, ч')
 
